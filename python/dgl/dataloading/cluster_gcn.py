422a00630027597f61c99750ca4a7da856298cc8
dyyfk <36778268+dyyfk@users.noreply.github.com>
2022-05-23 17:49:36 -0700

    Update cluster_gcn.py

--- python/dgl/dataloading/cluster_gcn.py
+++ python/dgl/dataloading/cluster_gcn.py
@@ -8,7 +8,7 @@ from ..base import DGLError, BlockSampler
 from ..partition import metis_partition_assignment
 from .base import set_node_lazy_features, set_edge_lazy_features, Sampler
 
-class ClusterGCNSampler(BlockSampler):
+class ClusterGCNSampler(Sampler):
     """Cluster sampler from `Cluster-GCN: An Efficient Algorithm for Training
     Deep and Large Graph Convolutional Networks
     <https://arxiv.org/abs/1905.07953>`__
@@ -120,18 +120,18 @@ class ClusterGCNSampler(BlockSampler):
         return sg
 
     
-    def sample_blocks(self, g, seed_nodes, exclude_eids=None):
-        output_nodes = seed_nodes
-        blocks = []
-        for fanout in reversed(self.fanouts):
-            frontier = g.sample_neighbors(
-                seed_nodes, fanout, edge_dir=self.edge_dir, prob=self.prob,
-                replace=self.replace, output_device=self.output_device,
-                exclude_edges=exclude_eids)
-            eid = frontier.edata[EID]
-            block = to_block(frontier, seed_nodes)
-            block.edata[EID] = eid
-            seed_nodes = block.srcdata[NID]
-            blocks.insert(0, block)
+#     def sample_blocks(self, g, seed_nodes, exclude_eids=None):
+#         output_nodes = seed_nodes
+#         blocks = []
+#         for fanout in reversed(self.fanouts):
+#             frontier = g.sample_neighbors(
+#                 seed_nodes, fanout, edge_dir=self.edge_dir, prob=self.prob,
+#                 replace=self.replace, output_device=self.output_device,
+#                 exclude_edges=exclude_eids)
+#             eid = frontier.edata[EID]
+#             block = to_block(frontier, seed_nodes)
+#             block.edata[EID] = eid
+#             seed_nodes = block.srcdata[NID]
+#             blocks.insert(0, block)
 
-        return seed_nodes, output_nodes, blocks
+#         return seed_nodes, output_nodes, blocks
